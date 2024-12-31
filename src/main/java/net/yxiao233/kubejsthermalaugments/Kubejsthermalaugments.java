2ea2c71676cef68d5bf9a2c9a0b80d1c01d592e2
Y-Xiao233 <1870377449@qq.com>
2024-12-31 20:46:56 +0800

    init

--- src/main/java/net/yxiao233/kubejsthermalaugments/Kubejsthermalaugments.java
+++ src/main/java/net/yxiao233/kubejsthermalaugments/Kubejsthermalaugments.java
@@ -1,11 +1,8 @@
 package net.yxiao233.kubejsthermalaugments;
 
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Kubejsthermalaugments.MODID)
@@ -16,13 +13,5 @@ public class Kubejsthermalaugments {
     public Kubejsthermalaugments() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
-
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-        }
     }
 }
